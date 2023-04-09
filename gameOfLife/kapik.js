1d26987ce041c812b9f48b22f2222db13cb06cb8
Styop08 <styopa.soghoyan@tumo.org>
2023-04-09 19:34:25 +0400

    Js class inheritance

--- gameOfLife/kapik.js
+++ gameOfLife/kapik.js
@@ -1,7 +1,6 @@
-class Kapik {
+class Kapik extends LivingCreature{
     constructor(x, y) {
-        this.x = x
-        this.y = y
+       super(x,y)
         this.energy = 20
         this.directions = [];
     }
@@ -23,21 +22,7 @@ class Kapik {
 
     chooseCell(char) {
         this.getNewCoordinates()
-        let found = []
-
-
-        for (let i in this.directions) {
-            let x = this.directions[i][0]
-            let y = this.directions[i][1]
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == char) {
-                    found.push(this.directions[i])
-                }
-            }
-        }
-
-
-        return found
+       return super.chooseCell(char)
 
     }
 
